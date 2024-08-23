e866781684d8212572154637092fbdceb7c57cfe
blin4504 <bl4504@g.rit.edu>
2024-08-22 21:43:13 -0400

    feat: basic add, list, remove, complete, and quit functionality

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 type Todo struct {
@@ -11,29 +14,66 @@ type Todo struct {
 	complete bool
 }
 
-var todoList []Todo
+func (todo Todo) String() string {
+	return fmt.Sprintf("%d %s %t", todo.id, todo.task, todo.complete)
+}
+
+var todoList map[int]Todo = make(map[int]Todo)
+
+func add(task []string) {
+	id := len(todoList) + 1
+	newTodo := Todo{id, strings.Join(task, " "), false}
+	todoList[id] = newTodo
+}
+
+func list() {
+	for _, todo := range todoList {
+		fmt.Println(todo)
+	}
+}
+
+func remove(id int) {
+	delete(todoList, id)
+}
+
+func complete(id int) {
+	old := todoList[id]
+	todoList[id] = Todo{id, old.task, true}
+}
 
 func parseCommand(args []string) {
 	command := args[0]
 	switch command {
-	case "add":
-		fmt.Println("add commmnd")
-	case "list":
-		fmt.Println("list command")
-	case "remove":
-		fmt.Println("remove command")
-	case "complete":
-		fmt.Println("complete command")
+	case "add": // add <task>
+		add(args[1:])
+	case "list": // list
+		list()
+	case "remove": // remove <id>
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			panic(err)
+		}
+		remove(id)
+	case "complete": // complete <id>
+		id, err := strconv.Atoi(args[1])
+		if err != nil {
+			panic(err)
+		}
+		complete(id)
+	case "quit":
+		os.Exit(1)
+	default:
+		fmt.Println("Usage: todo-cli > <command> <args>")
 	}
 }
 
 func main() {
-	args := os.Args
-
-	if len(args) < 3 {
-		fmt.Println("Usage: ./todo-cli <command> <args>")
-		return
+	for {
+		fmt.Print("todo-cli > ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		inp := scanner.Text()
+		tokens := strings.Split(inp, " ")
+		parseCommand(tokens)
 	}
-
-	parseCommand(args[1:])
 }
